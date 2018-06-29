14300020c1fb3ff796af095048b88ad562ec35a8
mouadcherkaoui <cherkaoui.mouad@live.fr>
2018-06-29 01:54:22 +0100

    passport auth flow integration 1st step

--- functions/src/services/users.service.ts
+++ functions/src/services/users.service.ts
@@ -19,33 +19,33 @@ export class UsersService {
         return admin.firestore().doc(`fbusers/${email}`).get();//.then(u => u.data());
     }
 
-    async upsertFbUser(profile: Profile, accessToken: string) {
-        let resultToReturn: fbUserDto;
-        let existingUser = this.findOneByEmail(profile.id).then(u => resultToReturn = u.data() as fbUserDto).catch(e => console.log(e));
-        try {
-          if (!existingUser) {
-            let savedUser = await this.saveUser({
-              name: profile.displayName,
-              email: profile.emails[0].value || null,
-              avatarUrl: profile.photos[0].value || null,
-              fb_id: profile.id,
-              fb_token: accessToken
-            });
-            if (savedUser) {
-              return savedUser;
-            } else {
-              return null;
-            }
-          }
-          else {
-            return existingUser;
-          }
-        } catch (e) {
-          console.log('[user.svc->upsertFbUser()');
-          console.dir(e);
-          return null;
-        }    
-    }
+    // async upsertFbUser(profile: Profile, accessToken: string) {
+    //     let resultToReturn: fbUserDto;
+    //     let existingUser = this.findOneByEmail(profile.id).then(u => resultToReturn = u.data() as fbUserDto).catch(e => console.log(e));
+    //     try {
+    //       if (!existingUser) {
+    //         let savedUser = await this.saveUser({
+    //           name: profile.displayName,
+    //           email: profile.emails[0].value || null,
+    //           avatarUrl: profile.photos[0].value || null,
+    //           fb_id: profile.id,
+    //           fb_token: accessToken
+    //         });
+    //         if (savedUser) {
+    //           return savedUser;
+    //         } else {
+    //           return null;
+    //         }
+    //       }
+    //       else {
+    //         return existingUser;
+    //       }
+    //     } catch (e) {
+    //       console.log('[user.svc->upsertFbUser()');
+    //       console.dir(e);
+    //       return null;
+    //     }    
+    // }
     
     saveUser(user) { }
 }
